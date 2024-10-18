68596f03acddc41c150745a91bce0fff48cedf52
NebojsaOgnjanovic <nebojsaog@gmail.com>
2024-10-18 15:40:00 +0200

    Adding basic UI for login page

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,11 +1,59 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Button,
+  Text,
+  StyleSheet,
+  Alert,
+  Image,
+  Dimensions,
+} from "react-native";
+import QWInput from "../componenets/QWInput";
+
+const { width, height } = Dimensions.get("window");
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {};
+
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/qwertify.png")}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <Text style={styles.title}>Login</Text>
+      <QWInput placeholder="Email" value={email} onChangeText={setEmail} />
+      <QWInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  logo: {
+    width: width * 0.8,
+    height: height * 0.3,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+});
 
 export default LoginScreen;
